99b06d7474ec9b97f0755302385f8edca92cab50
Josh Levinger <josh@levinger.net>
2013-12-26 11:46:17 +0200

    initial commit

---
+++ load_puppies.js
@@ -0,0 +1,25 @@
+// concept to hide iframe and parse in js from @sarim
+// http://blog.sarim.me/2013/09/06/using-twitters-embedded-timeline-as-data-source/
+
+function parsetwitter(){
+    frms = document.getElementsByTagName("iframe");
+    for (i=0;i<frms.length;++i){
+        if (frms[i].id.search("twitter-widget-") != -1){
+            tweets = frames[frms[i].id].document.body.getElementsByClassName("e-entry-content");
+            if (tweets.length === 0) return setTimeout(parsetwitter,500);
+            
+            tweetmeta = frames[frms[i].id].document.body.getElementsByClassName("permalink");
+            content = "";
+            
+            for (p=0;p<tweets.length;++p){
+                txt = tweets[p].innerText;
+                link = tweetmeta[p].href;
+                media = tweets[p].getElementsByTagName('img');
+                content += '<img src="'+media[0].src+'" alt="'+txt+'">';
+                
+            }
+            
+            document.getElementById('puppy').innerHTML = content;
+        }
+    }
+}
